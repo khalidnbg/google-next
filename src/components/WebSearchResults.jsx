a32e7a0f1c191c2c449452831b569eb6d282058a
khalidnbg <khalidnabgaoui4@gmail.com>
2024-03-27 13:46:10 +0000

    add pagination component

--- src/components/WebSearchResults.jsx
+++ src/components/WebSearchResults.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Parser from "html-react-parser";
+import PaginationButtons from "./PaginationsButton";
 
 export default function WebSearchResults({ results }) {
   return (
@@ -21,6 +22,7 @@ export default function WebSearchResults({ results }) {
           <p className="text-gray-600">{Parser(result.htmlSnippet)}</p>
         </div>
       ))}
+      <PaginationButtons />
     </div>
   );
 }

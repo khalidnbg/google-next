be648cfc1bc03857f8cace6ff1e575631c55368a
khalidnbg <khalidnabgaoui4@gmail.com>
2024-03-27 10:58:34 +0000

    complete the search box component

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -13,8 +13,8 @@ export default function SearchHeader() {
           <Image
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/800px-Google_2015_logo.svg.png"
             alt="Google Logo"
-            width={300}
-            height={100}
+            width={120}
+            height={40}
             priority
             style={{ width: "auto", height: "auto" }}
           />
